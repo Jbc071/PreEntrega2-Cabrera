f0d7713ff6b295163225a3588cc7ccfb953c05ca
Jose Bernardo Cabrera <flaxx071@gmail.com>
2024-05-27 22:25:31 -0300

    primera entrega js

---
+++ index.js
@@ -0,0 +1,10 @@
+/* Funcion para generar un numero aleatorio entre 1 y 10*/
+
+
+function NumeroAleatorio(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+/* Ejecuta el simulador */
+
+simuladorAdivi();
